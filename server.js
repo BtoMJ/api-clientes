8900b954c008350974923b7e0424e685fc35fcbc
BtoMJ <bto.mena.17@gmail.com>
2024-06-01 10:52:57 -0600

    Modificación de rutas 2

--- server.js
+++ server.js
@@ -15,17 +15,17 @@ const port = process.env.PORT || 3001;
 app.use( express.json() );
 app.use( '/api', userRoutes );
 app.use( bodyParser.json() );
-app.use( bodyParser.urlencoded( { extended: 'true'} ) );
+app.use( bodyParser.urlencoded( { extended: 'false'} ) );
 app.use(cors);
 app.use(corsc());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
-    next();
-  });
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
+//     next();
+//   });
 
 
 mongoose.set('strictQuery', false);
