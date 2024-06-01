f753f363a4645c673b63ab0d6f091d3f3970eae8
BtoMJ <bto.mena.17@gmail.com>
2024-06-01 01:13:05 -0600

    Modificación de rutas

--- server.js
+++ server.js
@@ -19,6 +19,14 @@ app.use( bodyParser.urlencoded( { extended: 'true'} ) );
 app.use(cors);
 app.use(corsc());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
+    next();
+  });
+
 
 mongoose.set('strictQuery', false);
 
